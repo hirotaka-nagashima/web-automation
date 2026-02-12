28103e78af74149cfb818333b10a80e017823fa2
Hirotaka Nagashima <62758713+hirotaka-nagashima@users.noreply.github.com>
2026-02-13 00:18:49 +0900

    fix: Update path

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+import os
+
 from browser import chrome
 from utils import sms
 
 
 def main():
-    chrome_user_data_dir = r"C:\~"
+    chrome_user_data_dir = os.path.abspath(__file__ + "/../../data/chrome")
     browser = chrome.Chrome(absolute_user_data_dir=chrome_user_data_dir)
     sms_ = sms.SMS(browser)
     sms_.connect()
